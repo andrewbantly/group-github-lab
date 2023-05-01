33ff20131a17e1689693f3c89709da8e53777259
Powwow84 <pao.saelee84@gmail.com>
2023-05-01 14:37:48 -0700

    completed main route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+import Home from "./components/Home"
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route 
+            path="/"
+            element={<Home />}
+            />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
