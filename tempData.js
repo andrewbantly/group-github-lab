fbede95133a7501005dcea98f4938e97f7325536
andrewbantly <andrewbantly@gmail.com>
2023-05-01 14:31:53 -0700

    temp data created

---
+++ tempData.js
@@ -0,0 +1,14 @@
+const team = [{
+    name: "Abdallah",
+    bio: `Throughout my time at the warehouse, I developed a passion for technology and decided to pursue a career in software engineering. I am currently in the process of obtaining a software engineering certificate at General Assembly, where I have learned valuable skills such as JavaScript, HTML/CSS, and SQL. I am eager to continue my education and am excited to soon learn Python.`,
+    image: "https://media.licdn.com/dms/image/D4E03AQEdkw7-T2ImjA/profile-displayphoto-shrink_800_800/0/1680196090361?e=1688601600&v=beta&t=QKlF7jq6ljaJNjuL2JRJRJUFi5eJgaRYB3IFfqfBtnY"
+}, {
+    name: "Pao",
+    bio: "Pao's needs to put his personal brand statement on his LinkedIn!",
+    image: "https://media.licdn.com/dms/image/D5635AQFnCPrBOgpnaA/profile-framedphoto-shrink_800_800/0/1677180840662?e=1683583200&v=beta&t=gKVgvPXbJfDSwRNpjIIRnpDPgbL-DkOu0gMmcpm0DAs"
+}, {
+    name: "Andrew",
+    bio: "As a determined software engineer, I am driven by the endless opportunities that lie ahead. I bring six years of valuable media production experience and possess a unique blend of skills that enables me to approach problem-solving with a fresh perspective. I am ready and eager to deliver innovative solutions and make a meaningful impact on the software development life cycle.",
+    image: "https://media.licdn.com/dms/image/D5603AQEICARRnoENhQ/profile-displayphoto-shrink_800_800/0/1677704970769?e=1688601600&v=beta&t=5K8YcfHNN3OvcQlanMOVQ0Op6w2yO5GEo2ohlGAZS5A"
+}
+]
