5550f6fb2d69d032ee18842d4db3179d889ca16c
abdallah1936 <awadallaabdallah@gmail.com>
2023-05-01 15:24:05 -0700

    Link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import gitTogetherTeam from '../tempData';
 
 const Card = ({ image, name, info }) => (
-  <div className="homeCard">
-    <img className="homeCardImage" src={image} alt={name} />
-    <h2 className="homeCardName">{name}</h2>
-    <p className="homeCardInfo">{info}</p>
-  </div>
+  <Link to={`/profile/${name}`} className="homeCardLink">
+    <div className="homeCard">
+      <img className="homeCardImage" src={image} alt={name} />
+      <h2 className="homeCardName">{name}</h2>
+      <p className="homeCardInfo">{info}</p>
+    </div>
+  </Link>
 );
 
 const Home = () => {
