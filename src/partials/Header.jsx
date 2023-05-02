de9ab7d77ea3663233a7c6c3a83dc383c5c6ddbd
abdallah1936 <awadallaabdallah@gmail.com>
2023-05-01 23:38:30 -0700

    header style

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,13 +1,14 @@
-import {Link} from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-    return(
-        <div>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="headerContainer">
+      <ul className="headerNav">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
